af7469865d828f5df2022334599cfe53f2ed71ba
Kunal Deshmukh <kunal.deshmukh@siemens.com>
2017-09-21 11:13:04 +0530

    Add project files.

---
+++ JsonDiff/Program.cs
@@ -0,0 +1,29 @@
+﻿using JsonDiff.SimpleHelpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var originalObj = File.ReadAllText("source.json");
+            var updatedObj = File.ReadAllText("target.json");
+
+            var diff = ObjectDiffPatch.GenerateDiff(JsonConvert.DeserializeObject(originalObj.ToString()), JsonConvert.DeserializeObject(updatedObj.ToString()));
+
+            // original properties values
+            Console.WriteLine(diff.OldValues.ToString());
+
+            // updated properties values
+            Console.WriteLine(diff.NewValues.ToString());
+
+        }
+    }
+}
---
+++ JsonDiff/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JsonDiff")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Siemens")]
+[assembly: AssemblyProduct("JsonDiff")]
+[assembly: AssemblyCopyright("Copyright © Siemens 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7a5179dc-6c38-41c5-9540-6b0272801dee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
